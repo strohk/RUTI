5d8276169b4031e02f71c50644335c79cdadde6e
strohk <rohkohl@informatik.uni-halle.de>
2011-12-22 19:19:20 +0100

    First Commit

---
+++ app/helpers/ruti_helper.rb
@@ -0,0 +1,2 @@
+module RutiHelper
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RUTI::Application.config.secret_token = 'c97deec2e3bc88df0b8574fdbc82d82569c6677fe27d32a37b471b33d439f15358730e619a14372960f8445baf828044e2d94e75cbb61cd119357dfbd116e327'
